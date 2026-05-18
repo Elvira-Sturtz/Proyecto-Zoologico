8222e5155eed3c147deea0bdfe21677a70bbc224
Elvira-Sturtz <elvira.sturtz@alu.inspt.utn.edu.ar>
2026-05-18 18:04:54 -0300

    conexión a database en MongoDB Atlas

--- backend/src/database.js
+++ backend/src/database.js
@@ -1,13 +1,19 @@
-const mongoose = require('mongoose') 
+const mongoose = require("mongoose");
 
-// cadena de conexion 
-const URI = process.env.MONGODB_URI
-            ? process.env.MONGODB_URI
-            : 'mongodb://localhost/dbtest'  
+// cadena de conexion
 
-mongoose.connect(URI)
-    .then(() => console.log('Base de datos conectada:', URI))
-    .catch(err => console.error('Error de conexión:', err));
+const connectDB = async () => {
+  try {
+    const uri =
+      process.env.MONGODB_URI || "mongodb://localhost:27017/datazoologico";
 
+    await mongoose.connect(uri);
 
+    console.log(" DB conectada", uri);
+  } catch (error) {
+    console.error(" Error al conectar MongoDB:", error.message);
+    process.exit(1);
+  }
+};
 
+module.exports = connectDB;
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,15 +1,15 @@
-require('dotenv').config()
+require("dotenv").config();
 
-const app = require('./app') 
+const app = require("./app");
 
-require('./database')
+const connectDB = require("./database");
 
-//esta logica es para ejecutar el servidor 
-async function main() {  
-    await app.listen(app.get('port')) 
-    console.log('el servidor se esta ejecutando en el puerto: ', app.get('port')); 
+//esta logica es para ejecutar el servidor
+async function main() {
+  await connectDB();
 
-    
+  await app.listen(app.get("port"));
+  console.log("el servidor se esta ejecutando en el puerto: ", app.get("port"));
 }
 
-main(); 
+main();
