cf50d7aaf7c2deeb9013121efdf8a61fde113fe4
Elvira-Sturtz <elvira.sturtz@alu.inspt.utn.edu.ar>
2026-05-19 19:56:42 -0300

    creando token

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,17 +1,34 @@
 const Usuario = require("../models/Usuario");
 
+const { creaateAccessToken, createAccessToken } = require("../libs/jwt");
+
+const bcrypt = require("bcryptjs");
+
 const register = async (req, res) => {
   try {
     const { email, password, username } = req.body;
 
+    // hashing the password
+    const passwordHash = await bcrypt.hash(password, 10);
+
     const newUsuario = new Usuario({
       username,
       email,
-      password,
+      password: passwordHash,
     });
 
-    await newUsuario.save();
-    res.status(201).json({ message: "Usuario creado correctamente" });
+    // saving the user in the database
+    const userSaved = await newUsuario.save();
+
+    const token = await createAccessToken({ id: userSaved._id });
+
+    res.cookie("token", token);
+    res.status(201).json({
+      message: "Usuario creado correctamente",
+      id: userSaved._id,
+      username: userSaved.username,
+      email: userSaved.email,
+    });
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
---
+++ backend/src/libs/jwt.js
@@ -0,0 +1,10 @@
+const jwt = require("jsonwebtoken");
+
+export async function createAccessToken(payload) {
+  return new Promise((resolve, reject) => {
+    jwt.sign(payload, TOKEN_SECRET, { expiresIn: "1d" }, (err, token) => {
+      if (err) reject(err);
+      resolve(token);
+    });
+  });
+}
